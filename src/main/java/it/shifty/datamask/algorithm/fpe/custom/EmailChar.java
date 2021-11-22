9a96ac99593ddfef7bfb75d178373fa720478b53
shiftyita <54118429+shiftyita@users.noreply.github.com>
2021-11-22 22:19:47 +0100

    Reformat classes and order

--- src/main/java/it/shifty/datamask/algorithm/fpe/custom/EmailChar.java
+++ src/main/java/it/shifty/datamask/algorithm/fpe/custom/EmailChar.java
@@ -5,8 +5,6 @@ import com.idealista.fpe.config.Alphabet;
 public class EmailChar implements Alphabet {
   private static final char[] CHARS = new char[]
       {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-     // 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-     // '!','#','$','%','&','*','+','-','/','=','?','^','_','`','{','|','}','~',
       '0','1','2','3','4','5','6','7','8','9'};
 
   public EmailChar() {
